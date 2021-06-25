e28326b0009ae08a7b2655939b6020120abca405
Cypher <81826946+Unknown079@users.noreply.github.com>
2021-06-25 17:10:18 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-import time, pyautogui
+import time
+import pyautogui
 
-#you can also configure the code if u wanna to, i made just a simple one :)
-i = 0
-x = [0, 10, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
-
+x = pyautogui.prompt("Type something: (You'll have 5 seconds time)\n", "SPAMBOT")
 time.sleep(5)
+
 while True:
-    pyautogui.write(str(i))
+    ban = x
+    pyautogui.write(ban)
     pyautogui.press("enter")
-    if i in x:
-        pyautogui.write("U are gay")
-        pyautogui.press("enter")
-    i += 1
+    
